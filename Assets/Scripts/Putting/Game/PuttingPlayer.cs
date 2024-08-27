125491c6f17e4c37801dfe99344b9a580a1dd05b
Ellevern <elisavandk@gmail.com>
2024-08-27 05:08:52 -0400

    Made it so the golf putter despawns when released by player

--- Assets/Scripts/Putting/Game/PuttingPlayer.cs
+++ Assets/Scripts/Putting/Game/PuttingPlayer.cs
@@ -11,25 +11,66 @@ namespace Minigolf.Putting.Game
         private XRInteractionManager interactionManager;
         [SerializeField]
         private GolfBall playerGolfBall;
+        // Consider making it so the GolfPutter class can be used to access its Grab Interactable class
         [SerializeField]
         private XRGrabInteractable putterPrefab;
         [SerializeField]
         private XRDirectInteractor rDirectInteractor;
         [SerializeField]
+        private XRDirectInteractor lDirectInteractor;
+        [SerializeField]
         private InputAction rightGripPressed;
+        [SerializeField]
+        private InputAction leftGripPressed;
 
         public GolfBall PlayerGolfBall => playerGolfBall;
 
-        private void OnEnable()
+        private GolfPutter spawnedPutter;
+
+        private void Awake()
         {
             rightGripPressed.Enable();
+            leftGripPressed.Enable();
+        }
+
+        private void OnEnable()
+        {
             rightGripPressed.started += SpawnPutterRightHand;
+            leftGripPressed.started += SpawnPutterLeftHand;
         }
 
         private void OnDisable()
         {
             rightGripPressed.started -= SpawnPutterRightHand;
+            leftGripPressed.started -= SpawnPutterLeftHand;
             rightGripPressed.Disable();
+            leftGripPressed.Disable();
+        }
+
+        private void Update()
+        {
+            if (!rDirectInteractor.hasSelection ||  !lDirectInteractor.hasSelection)
+            {
+                PutterReleased();
+            } 
+        }
+
+        private void SpawnPutter(IXRSelectInteractor handInteractor)
+        {
+            if (spawnedPutter != null) return;
+
+            XRGrabInteractable putterInteractable = Instantiate(putterPrefab, handInteractor.transform.position, handInteractor.transform.rotation);
+            spawnedPutter = putterInteractable.GetComponent<GolfPutter>();
+            interactionManager.SelectEnter(handInteractor, putterInteractable);
+        }
+
+        private void PutterReleased()
+        {
+            if (spawnedPutter == null) return;
+            if (spawnedPutter.IsSelected()) return;
+
+            spawnedPutter.DespawnPutter();
+            spawnedPutter = null;
         }
 
         private void SpawnPutterRightHand(InputAction.CallbackContext ctx)
@@ -37,12 +78,18 @@ namespace Minigolf.Putting.Game
             if (rDirectInteractor.hasHover) return;
             if (rDirectInteractor.hasSelection) return;
 
-            XRGrabInteractable spawnedPutter = Instantiate(putterPrefab, rDirectInteractor.transform.position, rDirectInteractor.transform.rotation);
-            interactionManager.SelectEnter(rDirectInteractor, spawnedPutter);
-            
+            SpawnPutter(rDirectInteractor);
         }
 
-        
+        private void SpawnPutterLeftHand(InputAction.CallbackContext ctx)
+        {
+            if (lDirectInteractor.hasHover) return;
+            if (lDirectInteractor.hasSelection) return;
+
+            SpawnPutter(lDirectInteractor);
+        }
+
+
         // Player Hits Select Button on XR Controller:
         // Player Holding Something => return;
         // Player is Already Holding Putter in Other Hand => return;
--- Assets/Scripts/Putting/Interactable/GolfPutter.cs
+++ Assets/Scripts/Putting/Interactable/GolfPutter.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Minigolf.Putting.Interactable
 {
+    [RequireComponent(typeof(XRGrabInteractable))]
     public class GolfPutter : MonoBehaviour
     {
         private bool isHeld = false;
+
+        private XRGrabInteractable putterInteractable;
 
         public bool IsHeld
         {
             get => isHeld;
             set => isHeld = value;
+        }
+
+        private void Awake()
+        {
+            putterInteractable = GetComponent<XRGrabInteractable>();
+        }
+
+        public bool IsSelected() => putterInteractable.isSelected;
+
+        public void DespawnPutter()
+        {
+            Destroy(gameObject);
         }
     }
 }
