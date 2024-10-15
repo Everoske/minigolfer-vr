4ef474599d466307de97febe1e9b807e772500ee
Ellevern <elisavandk@gmail.com>
2024-10-15 14:29:39 -0400

    Added menu navigation in HandMenu class

--- Assets/Scripts/UI/HandMenu.cs
+++ Assets/Scripts/UI/HandMenu.cs
@@ -9,6 +9,57 @@ namespace Minigolf.UI
         [SerializeField]
         private NavUIButton[] navButtons;
 
+        private int activeNavButton = -1;
+
+        private void Start()
+        {
+            AssignIndices();
+            HandleSelectionActivated(0);
+        }
+
+        private void OnEnable()
+        {
+           EnableOnSelectionActivated();
+        }
+
+        private void OnDisable()
+        {
+            DisableOnSelectionActivated();
+        }
+
+        private void AssignIndices()
+        {
+            for (int i = 0; i < navButtons.Length; i++)
+            {
+                navButtons[i].Index = i;
+            }
+        }
+
+
+        private void EnableOnSelectionActivated()
+        {
+            for (int i = 0; i < navButtons.Length; i++)
+            {
+                navButtons[i].onSelectionActivated += HandleSelectionActivated;
+            }
+        }
+
+        private void DisableOnSelectionActivated()
+        {
+            for (int i = 0; i < navButtons.Length; i++)
+            {
+                navButtons[i].onSelectionActivated -= HandleSelectionActivated;
+            }
+        }
+
+        private void HandleSelectionActivated(int index)
+        {
+            if (index >= navButtons.Length) return;
+            if (activeNavButton > -1) navButtons[activeNavButton].DeactivateGroup();
+
+            activeNavButton = index;
+            navButtons[activeNavButton].ActivateGroup();
+        }
 
     }
 }
--- Assets/Scripts/UI/NavUIButton.cs
+++ Assets/Scripts/UI/NavUIButton.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Minigolf.UI
@@ -9,26 +10,37 @@ namespace Minigolf.UI
         [SerializeField]
         private GameObject selectionGroup;
 
+        public UnityAction<int> onSelectionActivated;
+
         private Button button;
+
+        public int Index
+        {
+            get => index;
+            set => index = value;
+        }
+
+        private int index = -1;
 
         private void Awake()
         {
             button = GetComponent<Button>();
+            DeactivateGroup();
         }
 
         private void OnEnable()
         {
-            button.onClick.AddListener(DeactivateGroup);
+            button.onClick.AddListener(HandleOnClick);
         }
 
         private void OnDisable()
         {
-            button.onClick.RemoveListener(DeactivateGroup);
+            button.onClick.RemoveListener(HandleOnClick);
         }
 
-        private void Start()
+        private void HandleOnClick()
         {
-            DeactivateGroup();
+            onSelectionActivated?.Invoke(index);
         }
 
         public void DeactivateGroup()
