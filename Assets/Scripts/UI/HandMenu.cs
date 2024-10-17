7bbddb6c2d6bc63d55f1726d092110081eb68e30
Ellevern <elisavandk@gmail.com>
2024-10-17 02:44:29 -0400

    Added navigation to all UI pages of hand menu

--- Assets/Scripts/UI/HandMenu.cs
+++ Assets/Scripts/UI/HandMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Minigolf.UI
 {
@@ -9,7 +10,27 @@ namespace Minigolf.UI
         [SerializeField]
         private NavUIButton[] navButtons;
 
-        private int activeNavButton = -1;
+        [SerializeField]
+        private GameObject menuContainer;
+
+        [SerializeField]
+        private GameObject mainMenuContainer;
+
+        [SerializeField]
+        private GameObject subMenuContainer;
+
+        [SerializeField]
+        private GameObject[] subMenus;
+
+        [SerializeField]
+        private GameObject quitMenuContainer;
+
+        [SerializeField]
+        private Button confirmQuitButton;
+
+        private int activePage = -1;
+
+        private int activeSubmenu = -1;
 
         private void Start()
         {
@@ -25,6 +46,80 @@ namespace Minigolf.UI
         private void OnDisable()
         {
             DisableOnSelectionActivated();
+        }
+
+        public void OpenHandMenu()
+        {
+            gameObject.SetActive(true);
+            HandleSelectionActivated(0);
+        }
+
+        public void OpenMenuContainer()
+        {
+            CloseQuitMenu();
+            menuContainer.SetActive(true);
+        }
+
+        public void OpenSubmenu(int index)
+        {
+            if (index > subMenus.Length) return;
+
+            mainMenuContainer.SetActive(false);
+            subMenuContainer.SetActive(true);
+
+            activeSubmenu = index;
+            subMenus[activeSubmenu].SetActive(true);
+        }
+
+        public void CloseSubmenus()
+        {
+            subMenus[activeSubmenu].SetActive(false);
+            subMenuContainer.SetActive(false);
+            mainMenuContainer.SetActive(true);
+        }    
+
+        public void CloseHandMenu()
+        {
+            subMenuContainer.SetActive(false);
+            CloseQuitMenu();
+            menuContainer.SetActive(true);
+            mainMenuContainer.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        public void OpenQuitToTitleConfirmation()
+        {
+            menuContainer.SetActive(false);
+            quitMenuContainer.SetActive(true);
+            confirmQuitButton.onClick.AddListener(QuitToTitle);
+        }
+
+        public void OpenQuitAppConfirmation()
+        {
+            menuContainer.SetActive(false);
+            quitMenuContainer.SetActive(true);
+            confirmQuitButton.onClick.AddListener(QuitApp);
+        }
+
+        
+
+        private void CloseQuitMenu()
+        {
+            if (!quitMenuContainer.activeInHierarchy) return;
+
+            confirmQuitButton.onClick.RemoveAllListeners();
+            quitMenuContainer.SetActive(false);
+        }
+
+        private void QuitToTitle()
+        {
+            Debug.Log("Quitting to title...");
+        }
+
+        private void QuitApp()
+        {
+            Debug.Log("Closing application...");
+            Application.Quit();
         }
 
         private void AssignIndices()
@@ -55,10 +150,10 @@ namespace Minigolf.UI
         private void HandleSelectionActivated(int index)
         {
             if (index >= navButtons.Length) return;
-            if (activeNavButton > -1) navButtons[activeNavButton].DeactivateGroup();
+            if (activePage > -1) navButtons[activePage].DeactivateGroup();
 
-            activeNavButton = index;
-            navButtons[activeNavButton].ActivateGroup();
+            activePage = index;
+            navButtons[activePage].ActivateGroup();
         }
 
     }
