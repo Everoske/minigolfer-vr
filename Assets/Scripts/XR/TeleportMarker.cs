859989350898feff8facf42c249b804cf03c58f7
Ellevern <elisavandk@gmail.com>
2024-11-12 02:38:02 -0500

    Updated poke interactor attach points

---
+++ Assets/Scripts/XR/TeleportMarker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Minigolf.XR
+{
+    public class TeleportMarker : MonoBehaviour
+    {
+        [SerializeField]
+        private LocomotionSystem locomotionSystem;
+        [SerializeField]
+        private TeleportationAnchor teleportAnchor;
+
+        public void TeleportToMarker()
+        {
+            
+        }
+    }
+}
