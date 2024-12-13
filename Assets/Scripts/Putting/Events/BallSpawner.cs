4b5f2a7b4ee8459776ce10181d71e71657e9e1f6
Ellevern <elisavandk@gmail.com>
2024-12-13 13:58:39 -0500

    Added randomization to the ball spawn script

--- Assets/Scripts/Putting/Events/BallSpawner.cs
+++ Assets/Scripts/Putting/Events/BallSpawner.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 
 namespace Minigolf.XR
 {
+    /// <summary>
+    /// Spawns Mini Golf Balls
+    /// </summary>
     public class BallSpawner : MonoBehaviour
     {
         [SerializeField]
-        private Transform spawnLocation;
+        private Transform[] ballSpawns;
 
+        /// <summary>
+        /// Spawns a mini golf ball
+        /// </summary>
+        /// <param name="ballPrefab">Mini golf ball prefab</param>
         public void SpawnBall(GolfBall ballPrefab)
         {
-            Instantiate(ballPrefab, spawnLocation);
+            Instantiate(ballPrefab, RandomSpawnLocation());
+        }
+
+        /// <summary>
+        /// Returns a random spawn location
+        /// </summary>
+        /// <returns>Random spawn transform</returns>
+        private Transform RandomSpawnLocation()
+        {
+            int index = Random.Range(0, ballSpawns.Length);
+            return ballSpawns[index];
         }
     }
 }
